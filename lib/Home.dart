5957df829d72c0d8c34715751f2d9e980f967308
ffelipedev <69429894+ffelipedev@users.noreply.github.com>
2020-08-09 15:08:30 -0300

    Update Home.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -14,36 +14,36 @@ class _HomeState extends State<Home> {
   void _recuperarPreco() async {
     String url = 'https://blockchain.info/ticker';
     http.Response response = await http
-        .get(url); //get faz a consulta na web, await aguarda a responsta
+        .get(url);
 
     Map<String, dynamic> retorno = json.decode(response.body);
     setState(() {
       _preco = retorno["BRL"]["buy"].toString();
     });
     print('Resultado: ' +
-        retorno["BRL"]["buy"].toString()); //recupera endereço da api
-  } //metodo para recuperar preço
+        retorno["BRL"]["buy"].toString());
+  } 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
           padding:
-              EdgeInsets.all(32), //da um espaçamento em todos os lados de 32
+              EdgeInsets.all(32), 
           child: Center(
-            //cenntraliza todo o conteudo no container
+            
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, //centraliza tudo
+              mainAxisAlignment: MainAxisAlignment.center, 
               children: [
-                Image.asset('imagens/bitcoin.png'), //cod para adicionar imagem
+                Image.asset('imagens/bitcoin.png'), 
                 Padding(
                   padding: EdgeInsets.only(
-                      top: 30, bottom: 30), // espaçamento entre imagem e botão
+                      top: 30, bottom: 30),
                   child: Text(
                     'R\$ ' + _preco,
-                    //para utlizar $ como texto precisa utlizar \ para funcionar como texto,
+                 
                     style:
-                        TextStyle(fontSize: 35), // adicionado estilo ao texto
+                        TextStyle(fontSize: 35),
                   ),
                 ),
                 RaisedButton(
@@ -52,9 +52,9 @@ class _HomeState extends State<Home> {
                     style: TextStyle(fontSize: 20, color: Colors.white),
                   ),
                   color: Colors.orange,
-                  // adicionando color
+               
                   padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
-                  // paddin adicionando o valor de espaço das areas
+                 
                   onPressed: _recuperarPreco,
                 ),
               ],
